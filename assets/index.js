93775b5782ea8fe52e5b1d81002b0039f23003bb
George Kirakosyan <georgiikirakosyan@mail.ru>
2022-06-16 12:52:52 +0300

    refactor js part of product page

--- assets/index.js
+++ assets/index.js
@@ -2,151 +2,178 @@
 
 console.log("You are a curious one, aren't you?");
 
-const QUANTITY_STEP = 1;
-const MIN_QUANTITY = 1;
-const MAX_QUANTITY = 9;
+(() => {
+  const QUANTITY_STEP = 1;
+  const MIN_QUANTITY = 1;
+  const MAX_QUANTITY = 9;
 
-const INPUT_DEFAULT_WIDTH = 40;
+  const INPUT_DEFAULT_WIDTH = 40;
 
-const fieldsetColor = document.querySelector(
-  '.order-details-form__fieldset--color'
-);
+  // COLOR
 
-const fieldsetColorLegendSpan = document.querySelector(
-  '.order-details-form__fieldset--color legend span'
-);
+  const colorFieldset = document.querySelector(
+    '.order-details-form__fieldset--color'
+  );
 
-const inputColorChecked = document.querySelector(
-  '.order-details-form__input-color:checked'
-);
+  const colorLegend = document.querySelector(
+    '.order-details-form__fieldset--color legend span'
+  );
 
-const buttonDecreaseQuantity = document.querySelector(
-  '.order-details-form__button-quantity--decrease'
-);
+  const colorInputChecked = document.querySelector(
+    '.order-details-form__input-color:checked'
+  );
 
-const buttonIncreaseQuantity = document.querySelector(
-  '.order-details-form__button-quantity--increase'
-);
+  colorLegend.textContent = colorInputChecked.value;
 
-const inputQuantity = document.querySelector(
-  '.order-details-form__input-quantity'
-);
+  colorFieldset.addEventListener('change', (evt) => {
+    if (evt.target.matches('.order-details-form__input-color')) {
+      colorLegend.textContent = evt.target.value;
+    }
+  });
 
-const quantityError = document.querySelector(
-  '.order-details-form__quantity-error'
-);
+  // DESCRIPTION
 
-const buttonAddToCart = document.querySelector(
-  '.order-details-form__button--add-to-cart'
-);
+  const descriptionWrapper = document.querySelector(
+    '.product-description__wrapper'
+  );
 
-const cart = document.querySelector(
-  '.site-navigation__item--cart span[aria-hidden="true"]'
-);
+  descriptionWrapper.addEventListener('click', (evt) => {
+    const { target } = evt;
 
-const cartHidden = document.querySelector(
-  '.site-navigation__item--cart span.visually-hidden'
-);
+    if (!target.matches('.product-description__button')) {
+      return;
+    }
 
-const formOrderDetails = document.querySelector('.order-details-form');
+    const id = target.getAttribute('aria-controls');
+    const description = document.getElementById(id);
+    const isExpanded =
+      target.getAttribute('aria-expanded') === 'true' ? true : false;
 
-const productDescriptionWrapper = document.querySelector(
-  '.product-description__wrapper'
-);
+    description.style.display = isExpanded ? 'none' : 'block';
+    target.setAttribute('aria-expanded', !isExpanded);
+  });
 
-fieldsetColorLegendSpan.textContent = inputColorChecked.value;
+  // QUANTITY
 
-function showError(message) {
-  quantityError.textContent = message;
-  quantityError.classList.add('active');
-}
+  const quantityError = document.querySelector(
+    '.order-details-form__quantity-error'
+  );
 
-function hideError() {
-  quantityError.textContent = '';
-  quantityError.classList.remove('active');
-}
+  function showError() {
+    quantityError.textContent = `You can only select a value between ${MIN_QUANTITY} and ${MAX_QUANTITY}.`;
+    quantityError.classList.add('active');
+  }
 
-buttonDecreaseQuantity.addEventListener('click', () => {
-  if (Number(inputQuantity.value) <= MIN_QUANTITY) {
-    showError(
-      `You can only select a value that is no less than ${MIN_QUANTITY}.`
-    );
-  } else {
-    inputQuantity.value = Number(inputQuantity.value) - QUANTITY_STEP;
+  function hideError() {
+    quantityError.textContent = '';
+    quantityError.classList.remove('active');
+  }
+
+  const inputQuantity = document.querySelector(
+    '.order-details-form__input-quantity'
+  );
+
+  function setInvalidStyle(isInvalid) {
+    if (isInvalid) {
+      inputQuantity.style.width = `${INPUT_DEFAULT_WIDTH * 2}px`;
+    } else {
+      inputQuantity.removeAttribute('style');
+    }
+  }
+
+  function getQuantity() {
+    return Number(inputQuantity.value);
+  }
+
+  function setQuantity(newValue) {
+    inputQuantity.value = newValue;
+  }
+
+  inputQuantity.addEventListener('input', (evt) => {
+    const quantity = Number(evt.target.value);
+
+    if (quantity < MIN_QUANTITY || quantity > MAX_QUANTITY) {
+      showError();
+      setInvalidStyle(true);
+    } else {
+      hideError();
+      setInvalidStyle(false);
+    }
+  });
+
+  const buttonDecreaseQuantity = document.querySelector(
+    '.order-details-form__button-quantity--decrease'
+  );
+
+  buttonDecreaseQuantity.addEventListener('click', () => {
+    const quantity = getQuantity();
+
+    if (quantity <= MIN_QUANTITY) {
+      showError();
+    } else if (quantity - QUANTITY_STEP > MAX_QUANTITY) {
+      setQuantity(quantity - QUANTITY_STEP);
+    } else {
+      setQuantity(quantity - QUANTITY_STEP);
+      hideError();
+      setInvalidStyle(false);
+    }
+  });
+
+  const buttonIncreaseQuantity = document.querySelector(
+    '.order-details-form__button-quantity--increase'
+  );
+
+  buttonIncreaseQuantity.addEventListener('click', () => {
+    const quantity = getQuantity();
+
+    if (quantity >= MAX_QUANTITY) {
+      showError();
+    } else if (quantity + QUANTITY_STEP < MIN_QUANTITY) {
+      setQuantity(quantity + QUANTITY_STEP);
+    } else {
+      setQuantity(quantity + QUANTITY_STEP);
+      hideError();
+      setInvalidStyle(false);
+    }
+  });
+
+  // ADD TO CART
+
+  const buttonAddToCart = document.querySelector(
+    '.order-details-form__button--add-to-cart'
+  );
+
+  const cart = document.querySelector(
+    '.site-navigation__item--cart span[aria-hidden="true"]'
+  );
+
+  const cartHidden = document.querySelector(
+    '.site-navigation__item--cart span.visually-hidden'
+  );
+
+  buttonAddToCart.addEventListener('click', () => {
+    const quantity = getQuantity();
+
+    if (quantity >= MIN_QUANTITY && quantity <= MAX_QUANTITY) {
+      hideError();
+      cart.textContent = `cart (${quantity})`;
+      cartHidden.textContent = `items in cart: ${quantity}`;
+    }
+  });
+
+  // FORM
+
+  const form = document.querySelector('.order-details-form');
+
+  form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
     hideError();
-  }
-});
 
-buttonIncreaseQuantity.addEventListener('click', () => {
-  if (Number(inputQuantity.value) >= MAX_QUANTITY) {
-    showError(
-      `You can only select a value that is no more than ${MAX_QUANTITY}.`
-    );
-  } else {
-    inputQuantity.value = Number(inputQuantity.value) + QUANTITY_STEP;
-    hideError();
-  }
-});
+    const data = new FormData(evt.target);
 
-inputQuantity.addEventListener('input', (evt) => {
-  const quantity = Number(evt.target.value);
-  if (quantity < MIN_QUANTITY || quantity > MAX_QUANTITY) {
-    evt.target.style.width = `${INPUT_DEFAULT_WIDTH * 2}px`;
-  } else {
-    evt.target.removeAttribute('style');
-  }
-});
-
-fieldsetColor.addEventListener('change', (evt) => {
-  if (evt.target.matches('.order-details-form__input-color')) {
-    fieldsetColorLegendSpan.textContent = evt.target.value;
-  }
-});
-
-buttonAddToCart.addEventListener('click', () => {
-  const quantity = Number(inputQuantity.value);
-
-  if (quantity < MIN_QUANTITY) {
-    showError(
-      `You can only select a value that is no less than ${MIN_QUANTITY}.`
-    );
-    inputQuantity.focus();
-  } else if (quantity > MAX_QUANTITY) {
-    showError(
-      `You can only select a value that is no more than ${MAX_QUANTITY}.`
-    );
-    inputQuantity.focus();
-  } else {
-    hideError();
-    cart.textContent = `cart (${inputQuantity.value})`;
-    cartHidden.textContent = `items in cart: ${inputQuantity.value}`;
-  }
-});
-
-formOrderDetails.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  hideError();
-
-  const data = new FormData(evt.target);
-
-  const size = data.get('size');
-  const color = data.get('color');
-  const quantity = Number(data.get('quantity'));
-  console.log({ size, color, quantity });
-});
-
-productDescriptionWrapper.addEventListener('click', (evt) => {
-  const { target } = evt;
-
-  if (!target.matches('.product-description__button')) {
-    return;
-  }
-
-  const id = target.getAttribute('aria-controls');
-  const description = document.getElementById(id);
-  const isExpanded =
-    target.getAttribute('aria-expanded') === 'true' ? true : false;
-
-  description.style.display = isExpanded ? 'none' : 'block';
-  target.setAttribute('aria-expanded', !isExpanded);
-});
+    const size = data.get('size');
+    const color = data.get('color');
+    const quantity = Number(data.get('quantity'));
+    console.log({ size, color, quantity });
+  });
+})();
